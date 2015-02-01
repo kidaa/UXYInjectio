20bda0187958bfe79da5933f52e3d448aca8856b
Heaven <uxyheaven@163.com>
2015-02-01 18:50:52 +0800

    update

--- UXYinjectioDemo/UXYInjectio/UXYInjectio.h
+++ UXYinjectioDemo/UXYInjectio/UXYInjectio.h
@@ -16,3 +16,10 @@
 - (BOOL)immediatelySaveInjectio;
 
 @end
+
+/*
+ 实现原理
+ 1 UXYInjectioHelper hook对象的快速消息转发方法(forwardingTargetForSelector),
+ 2 在快速消息转发的时候吧方法传递给SuiteName对应的UXYInjectio对象处理
+ 3 在UXYInjectio对象的标准消息转发方法里用NSUserDefaults对象处理set get方法
+*/
